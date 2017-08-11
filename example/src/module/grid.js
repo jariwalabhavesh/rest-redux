efd9c95bbc2124db130c423c84cf96e3cfbad326
bhaveshj <bhavesh@jumpbyte.com>
2017-08-11 14:34:06 +0530

    1.Done overview demo in griddle

--- example/src/module/grid.js
+++ example/src/module/grid.js
@@ -6,10 +6,18 @@ import { incompleteTodos } from '../api'
 class GridComponent extends Component {
   constructor(props) {
     super(props);
+    this.onFilter = this.onFilter.bind(this)
+    this.onSort = this.onSort.bind(this)
     // debugger;
   }
   componentWillReceiveProps(nextProps) {
     // debugger;
+  }
+  onFilter(filterText) {
+    console.log("filterText", filterText)
+  }
+  onSort(sortProperties) {
+    console.log("sortProperties", sortProperties)
   }
   render() {
     let { refresh, instances, next, prev, total, currentPage, gotoPage } = this.props
@@ -25,15 +33,15 @@ class GridComponent extends Component {
           data={instances}
           pageProperties={pageProperties}
           events={{
-            onFilter: (filterText) => { },
-            onSort: (sortProperties) => { },
+            onFilter: (filterText) => { this.onFilter(filterText) },
+            onSort: (sortProperties) => { this.onSort(sortProperties) },
             onNext: next,
             onPrevious: prev,
             onGetPage: (pageNumber) => gotoPage(pageNumber - 1),
           }} >
           <RowDefinition>
-            <ColumnDefinition id="id" title="id"  width={100}/>
-            <ColumnDefinition id="text" title="Desc"  width={100} />
+            <ColumnDefinition id="id" title="id" width={100} />
+            <ColumnDefinition id="text" title="Desc" width={100} />
           </RowDefinition>
         </Griddle>
         <p>
